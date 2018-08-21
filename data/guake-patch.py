1fe7e414d04e1147feabfa6efb21ca5479cd1488
Alessio Garzi <agarzi@clouditalia.com>
2018-08-21 15:59:53 +0200

    Included README.md in package and dropped guake-patch.py

--- data/guake-patch.py
+++
@@ -1,5 +0,0 @@
-    
-    @dbus.service.method(DBUS_NAME, in_signature='i', out_signature='s')
-    def get_gtktab_name(self, tab_index=0):
-        return self.guake.tabs.get_children()[tab_index].get_label()
-        
